d886f71a414f6826a04c2f4d8eb41b0a559b54db
Anmol Singh <anmol@kloudseven.com>
2018-01-13 22:09:11 -0500

    Merge conflict

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,6 +2,10 @@
   <div class="dashboard">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
+    <p v-on:click="download">File Download</p>
+    <div id="viewerContainer">
+      <div id="viewer" class="pdfViewer"></div>
+    </div>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -23,6 +27,7 @@
 
 <script>
 import firebase from 'firebase'
+import pdfjs from 'pdfjs-dist'
 
 export default {
   
@@ -38,6 +43,48 @@ export default {
       firebase.auth().signOut().then(function() {
         self.$router.replace('login');
       });
+    },
+    download: function() {
+      var storage = firebase.storage();
+      var pathReference = storage.ref('textbooks/CSC 444 Project --- Marketplace.pdf');
+
+      //pdfjs.PDFJS.workerSrc = '/../node_modules/pdfjs-dist/build/pdf.worker.entry.js';
+      pdfjs.PDFJS.workerSrc = '../../build/webpack/pdf.worker.entry.js';
+
+      var SEARCH_FOR = ''; // try 'Mozilla';
+
+      var container = document.getElementById('viewerContainer');
+
+      // (Optionally) enable hyperlinks within PDF files.
+
+      var pdfViewer = new pdfjs.PDFViewer({
+        container: container
+      });
+
+      // (Optionally) enable find controller.
+      var pdfFindController = new pdfjs.PDFFindController({
+        pdfViewer: pdfViewer
+      });
+      pdfViewer.setFindController(pdfFindController);
+
+      container.addEventListener('pagesinit', function () {
+        // We can use pdfViewer now, e.g. let's change default scale.
+        pdfViewer.currentScaleValue = 'page-width';
+
+        if (SEARCH_FOR) { // We can try search for things
+          pdfFindController.executeCommand('find', {query: SEARCH_FOR});
+        }
+      });
+
+      // Loading document.
+      pathReference.getDownloadURL().then(function(url) {
+        pdfjs.getDocument(url).then(function (pdfDocument) {
+          // Document loaded, specifying document for the viewer and
+          // the (optional) linkService.
+          pdfViewer.setDocument(pdfDocument);
+        });
+      });
+
     }
   }
 }
