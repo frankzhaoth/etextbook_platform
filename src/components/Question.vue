e94d9dbc5664fdc6f7d08482a21c43d6cf3b2e4b
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2018-02-04 15:50:27 -0500

    Initial implementation of adding answers to questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -4,6 +4,24 @@
       <v-card-title primary-title>{{ question }}</v-card-title>
       <v-card-text>{{ body }}</v-card-text>
     </v-card>
+
+    <v-card v-for="answer in answers" :key="answer.answer" color="grey lighten-4" flat>
+      <v-card-text>{{ answer.answer }}</v-card-text>
+    </v-card>
+
+    <v-card color="grey lighten-4" flat>
+      <v-subheader>Your Answer</v-subheader>
+        <v-card-text>
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field v-model="answer" label="Answer" textarea></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+      <v-btn @click="submitAnswer">Submit</v-btn>
+    </v-card>
 		
 	</div>
 </template>
@@ -19,12 +37,28 @@ export default {
     return {
       question: '',
       body: '',
-      userId: ''
+      userId: '',
+      answer: '',
+      answers: []
     }
   },
   
   methods: {
+    submitAnswer: function() {
+      let self = this;
+      let textbookId = self.$route.params.textbookId;
+      let questionId = self.$route.params.questionId;
+      let userId = firebase.auth().currentUser.uid;
 
+      if (userId != null) {
+        let aRef = firebase.database().ref('/forum/' + textbookId + '/' + questionId + '/answers/' + userId);
+        aRef.set({
+          'answer': self.answer
+        });
+      } else {
+        console.log("currentUser returned null");
+      }
+    }
   },
 
   created: function() {
@@ -33,14 +67,30 @@ export default {
     let questionId = self.$route.params.questionId;
 
     let qRef = firebase.database().ref('/forum/' + textbookId + '/' + questionId);
-    qRef.once('value', function(question) {
+    qRef.on('value', function(question) {
       if (question.exists()) {
         let questionData = question.val();
+        let answers = questionData.answers;
+
         self.question = questionData.question;
         self.body = questionData.body;
         self.userId = questionData.userId;
+
+        Object.keys(answers).forEach(function(answer) {
+          self.answers.push(answers[answer]);
+        });
+        console.log(self.answers);
       }
     });
+  },
+
+  beforeDestroy: function() {
+    let self = this;
+    let textbookId = self.$route.params.textbookId;
+    let questionId = self.$route.params.questionId;
+
+    let qRef = firebase.database().ref('/forum/' + textbookId + '/' + questionId);
+    qRef.off();
   }
 }
 </script>
