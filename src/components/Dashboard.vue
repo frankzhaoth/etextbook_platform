3d1c3400d119d3cbf22418153351371e84f5fec0
Anmol Singh <anmol@kloudseven.com>
2018-02-13 00:10:02 -0500

    Favorites connected with Firebase TODO - Display favorites

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -7,12 +7,12 @@
           <hr/>
         </v-flex>
         <v-flex xs6 sm3 md2 lg2 v-for="textbook in textbooks" :key="textbook.key">
-          <v-card v-on:click="clickTextbook(textbook.key)">
-            <v-card-media :src="textbook.cover" height="200px"></v-card-media>
+          <v-card>
+            <v-card-media v-on:click="clickTextbook(textbook.key)" :src="textbook.cover" height="200px"></v-card-media>
             <v-card-title>{{ textbook.title }}</v-card-title>
             <v-card-actions>
-                <!--<v-btn style="margin: 0 auto;" flat color="primary"><v-icon class="mr-2">fa-star-o</v-icon>Favourite</v-btn>-->
-                <v-btn v-on:click="viewTextbook(textbook.key)" style="margin: 0 auto;" color="primary">View</v-btn>
+                <v-icon v-on:click="toggleFavourite(textbook.key)" class="mr-2 ml-2">fa-star-o</v-icon>
+                <v-icon v-on:click="viewTextbook(textbook.key)" class="mr-2 ml-2">fa-eye</v-icon>
             </v-card-actions>
           </v-card>
         </v-flex>
@@ -30,6 +30,7 @@ export default {
   data () {
     return {
       textbooks: [],
+      favourites: []
     }
   },
   methods: {
@@ -43,14 +44,43 @@ export default {
     clickTextbook: function(key) {
       this.$router.push('/textbook/' + key);
     },
+    
     viewTextbook: function(key) {
       this.$router.push('/textbook/' + key + "/view");
     },   
+
+    toggleFavourite: function(_key) {
+      let currentUser = firebase.auth().currentUser.uid;
+
+      // Remove as favourite
+      if (this.favourites.indexOf(_key) > -1) {
+        this.favourites.splice(this.favourites.indexOf(_key), 1);
+        firebase.database().ref('/users/' + currentUser + '/favourites/').set(this.favourites);
+      }
+      // Set as favourite
+      else {
+        this.favourites.push(_key);
+        firebase.database().ref('/users/' + currentUser + '/favourites/').set(this.favourites);
+      }
+    },
+
   },
     
   created: function() {
     // Reads the textbooks from the database and populates the textbooks array
     let self = this;
+    let currentUser = firebase.auth().currentUser.uid;
+    
+    firebase.database().ref('/users/' + currentUser + '/favourites/').once('value')
+    .then(function(favourites) {
+      if (favourites.exists()) {
+        favourites.forEach(function(favourite) {
+          self.favourites.push(favourite.val());
+        })
+      }
+      console.log(self.favourites);
+    });
+
     firebase.database().ref('/textbooks').once('value')
     .then(function(textbooks) {
       if (textbooks.exists()) {
@@ -85,6 +115,14 @@ export default {
     margin-bottom: 0;
     margin-top: 20px;
     font-weight: 400;
+  }
+
+  #dashboard .card i {
+    cursor: pointer;
+  }
+
+  #dashboard .card i:hover {
+    color: #1976D2;
   }
 
   #dashboard .loader {
