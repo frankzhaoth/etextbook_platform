19df5e91b73c41ccf54b640e9b81ba18073d73a1
tzaitoun <talat.zaitoun@mail.utoronto.ca>
2018-02-21 05:51:52 -0500

    Added upvoting functionality

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -7,23 +7,27 @@
           
           <v-flex xs2 sm1 md1 lg1>
             <v-layout align-center column>
-              <i class="material-icons">arrow_drop_up</i>
-              <h6 class="subheading">2</h6>
-              <i class="material-icons">arrow_drop_down</i>
+              <v-btn flat :color="question.isUpvoted" icon slot="activator" @click="upvoteQuestion">
+                <v-icon large>fa-caret-up</v-icon>
+              </v-btn>
+              <h6 class="subheading">{{ question.voteScore }}</h6>
+              <v-btn flat :color="question.isDownvoted" icon slot="activator" @click="downvoteQuestion">
+                <v-icon large>fa-caret-down</v-icon>
+              </v-btn>
             </v-layout>
           </v-flex>
          
           <v-flex xs10 sm11 md11 lg11>
             <v-card-title primary-title>
-              <h5 class="headline">{{ question }}</h5>
+              <h5 class="headline">{{ question.question }}</h5>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              <h6 v-html="body" class="subheading"></h6>
+              <h6 v-html="question.questionBody" class="subheading"></h6>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <h6 class="caption ml-0">By: {{ userName }}</h6>
+              <h6 class="caption ml-0">By: {{ question.questionUserName }}</h6>
             </v-card-actions>
           </v-flex>
         
@@ -31,18 +35,24 @@
       </v-card>
     </v-container>
 
-    <v-container fluid v-if="answers.length > 0">     
+    <v-container fluid v-if="questionAnswers.length > 0">     
       <v-subheader>
         <h6 class="title">Answers</h6>
       </v-subheader>
 
-      <v-card v-for="answer in answers" :key="answer.answer" class="mb-3">
+      <v-card v-for="(answer, index) in questionAnswers" :key="answer.answerId" class="mb-3">
         <v-layout>
           <v-flex xs2 sm1 md1 lg1>
             <v-layout align-center column>
-              <i class="material-icons">arrow_drop_up</i>
-              <h6 class="subheading">2</h6>
-              <i class="material-icons">arrow_drop_down</i>
+              <v-btn flat :color="answer.isUpvoted" icon slot="activator" 
+              @click="upvoteAnswer(answer.answerId, index)">
+                <v-icon large>fa-caret-up</v-icon>
+              </v-btn>
+              <h6 class="subheading">{{ answer.voteScore }}</h6>
+              <v-btn flat :color="answer.isDownvoted" icon slot="activator" 
+              @click="downvoteAnswer(answer.answerId, index)">
+                <v-icon large>fa-caret-down</v-icon>
+              </v-btn>
             </v-layout>
           </v-flex>
 
@@ -64,10 +74,10 @@
         <h6 class="title">Your Answer</h6>
       </v-subheader>
       <v-card>
-        <vue-editor v-model="answer" :editorToolbar="customToolbar"></vue-editor>
+        <vue-editor v-model="currentUserAnswer" :editorToolbar="customToolbar"></vue-editor>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="submitAnswer">Submit</v-btn>
+          <v-btn flat color="pink darken-3" @click="submitAnswer">Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-container>
@@ -89,12 +99,12 @@ export default {
   
   data () {
     return {
+      currentUserId: '',
+      currentUserAnswer: '',
       question: '',
-      body: '',
-      userName: '',
-      userId: '',
-      answer: '',
-      answers: [],
+      questionAnswers: [],
+      voted: 'pink darken-3',
+      notVoted: 'grey',
       customToolbar: [  
         [{ 'header': [false, 1, 2, 3, 4, 5, 6, ] }],
         ['bold', 'italic', 'underline', 'strike'],        
@@ -111,45 +121,199 @@ export default {
   
   methods: {
     submitAnswer: function() {
-      let self = this;
-      let textbookId = self.$route.params.textbookId;
-      let questionId = self.$route.params.questionId;
-      let userId = firebase.auth().currentUser.uid;
+    
+      let textbookId = this.$route.params.textbookId;
+      let questionId = this.$route.params.questionId;
+      let userId = this.currentUserId;
 
-      if (userId != null) {
-        // Get the name of the user
-        firebase.database().ref('/users/' + userId).once('value').then(function(user) {
-          let userName = user.val().name;
-          let aRef = firebase.database().ref('/forum/' + textbookId + '/' + questionId + '/answers/' + userId);
-          aRef.set({
-            'userName': userName,
-            'answer': self.answer
-          });
+      // Get the name of the user
+      firebase.database().ref('/users/' + userId).once('value').then(function(user) {
+        let userName = user.val().name;
+        let aRef = firebase.database().ref('/forum/' + textbookId + '/' + questionId + '/answers/' + userId);
+        aRef.set({
+          'userName': userName,
+          'answer': this.answer
         });
+      });     
+    },
+
+    upvoteQuestion: function() {
+      let textbookId = this.$route.params.textbookId;
+      let questionId = this.$route.params.questionId;
+      let userId = this.currentUserId;
+
+      let ref = firebase.database().ref('/forum/' + textbookId + '/' + questionId + '/votes/' + userId);
+
+      // If the user clicks on the upvote button again, then we remove the upvote, otherwise we store it
+      if (this.question.isUpvoted === this.voted) {
+        ref.remove();
       } else {
-        console.log("currentUser returned null");
+        ref.set({
+          vote : 1  
+        });
       }
+    },
+
+    downvoteQuestion: function() {
+      let textbookId = this.$route.params.textbookId;
+      let questionId = this.$route.params.questionId;
+      let userId = this.currentUserId;
+
+      let ref = firebase.database().ref('/forum/' + textbookId + '/' + questionId + '/votes/' + userId);
+      
+      // If the user clicks on the downvote button again, then we remove the downvote, otherwise we store it
+      if (this.question.isDownvoted === this.voted) {
+        ref.remove();
+      } else {
+        ref.set({
+          vote : -1  
+        });
+      }
+    },
+
+    upvoteAnswer: function(answerId, index) {
+      let textbookId = this.$route.params.textbookId;
+      let questionId = this.$route.params.questionId;
+      let userId = this.currentUserId;
+
+      let ref = firebase.database().ref('/forum/' + textbookId + '/' + questionId + 
+        '/answers/' + answerId + '/votes/' + userId);
+      
+      // If the user clicks on the upvote button again, then we remove the upvote, otherwise we store it
+      if (this.questionAnswers[index].isUpvoted === this.voted) {
+        ref.remove();
+      } else {
+        ref.set({
+          vote : 1  
+        });
+      }
+    },
+
+    downvoteAnswer: function(answerId, index) {
+      let textbookId = this.$route.params.textbookId;
+      let questionId = this.$route.params.questionId;
+      let userId = this.currentUserId;
+
+      let ref = firebase.database().ref('/forum/' + textbookId + '/' + questionId + 
+        '/answers/' + answerId + '/votes/' + userId);
+      
+      // If the user clicks on the downvote button again, then we remove the downvote, otherwise we store it
+      if (this.questionAnswers[index].isDownvoted === this.voted) {
+        ref.remove();
+      } else {
+        ref.set({
+          vote : -1  
+        });
+      }
+    },
+
+    initializeQuestionVotes: function(votes) {
+      
+      let voteScore = 0;
+      let isUpvoted = this.notVoted;
+      let isDownvoted = this.notVoted;
+
+      // If the question has votes, add them up, and update the UI
+      if (votes != null) {
+        let self = this;
+        Object.keys(votes).forEach(function(voteKey) {
+          voteScore += votes[voteKey].vote;
+          if (self.currentUserId === voteKey) {
+            if (votes[voteKey].vote === 1) {
+              isUpvoted = self.voted;
+            } else {
+              isDownvoted = self.voted;
+            }
+          }
+        });
+      }
+
+      let questionVotes = 
+        {
+          'voteScore': voteScore, 
+          'isUpvoted': isUpvoted, 
+          'isDownvoted': isDownvoted
+        };
+      
+      return questionVotes;
+    },
+
+    initializeAnswerVotes: function(votes) {
+      
+      let voteScore = 0;
+      let isUpvoted = this.notVoted;
+      let isDownvoted = this.notVoted;
+
+      // If the answer has votes, add them up, and update the UI
+      if (votes != null) {
+        let self = this;
+        Object.keys(votes).forEach(function(voteKey) {
+          voteScore += votes[voteKey].vote;
+          if (self.currentUserId === voteKey) {
+            if (votes[voteKey].vote === 1) {
+              isUpvoted = self.voted;
+            } else {
+              isDownvoted = self.voted;
+            }
+          }
+        });
+      }
+
+      let answerVotes = 
+        {
+          'voteScore': voteScore, 
+          'isUpvoted': isUpvoted, 
+          'isDownvoted': isDownvoted
+        };
+
+      return answerVotes;
     }
   },
 
   created: function() {
+
+    let textbookId = this.$route.params.textbookId;
+    let questionId = this.$route.params.questionId;
+
+    // Get the current user id
+    this.currentUserId = firebase.auth().currentUser.uid;
+
     let self = this;
-    let textbookId = self.$route.params.textbookId;
-    let questionId = self.$route.params.questionId;
 
     let qRef = firebase.database().ref('/forum/' + textbookId + '/' + questionId);
     qRef.on('value', function(question) {
       if (question.exists()) {
+
+        // Clear the answer array, since a new change triggers another database read
+        self.questionAnswers = [];
+
         let questionData = question.val();
-        let answers = questionData.answers;
 
-        self.question = questionData.question;
-        self.body = questionData.body;
-        self.userName = questionData.userName;
-        self.userId = questionData.userId;
+        // Get the question-specfic data 
+        self.question = 
+        {
+          'question': questionData.question,
+          'questionBody': questionData.body,
+          'questionUserName': questionData.userName,
+          'questionUserId': questionData.userId
+        };
 
-        Object.keys(answers).forEach(function(answer) {
-          self.answers.push(answers[answer]);
+        // Get the votes of the question and initialize the question votes
+        let votes = questionData.votes;
+        let questionVotes = self.initializeQuestionVotes(votes);
+        self.question.voteScore = questionVotes.voteScore;
+        self.question.isUpvoted = questionVotes.isUpvoted;
+        self.question.isDownvoted = questionVotes.isDownvoted;
+
+        // Get the answers of the question, the votes of each answer, and initialize them
+        let answers = questionData.answers; 
+        Object.keys(answers).forEach(function(answerKey) {
+          answers[answerKey].answerId = answerKey;
+          let answerVotes = self.initializeAnswerVotes(answers[answerKey].votes);
+          answers[answerKey].voteScore = answerVotes.voteScore;
+          answers[answerKey].isUpvoted = answerVotes.isUpvoted;
+          answers[answerKey].isDownvoted = answerVotes.isDownvoted;
+          self.questionAnswers.push(answers[answerKey]);
         });
       }
     });
